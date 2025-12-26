c19d5f8a9a92f8dbdfa6c74491d997b53492c0e2
Yodgorbek Komilov <kyodgorbek@gmail.com>
2025-12-27 00:51:22 +0100

    finished project

--- app/src/main/java/com/example/codelearnapp/domain/usecase/CheckAchievementsUseCase.kt
+++ app/src/main/java/com/example/codelearnapp/domain/usecase/CheckAchievementsUseCase.kt
@@ -1,5 +1,7 @@
 package com.example.codelearnapp.domain.usecase
 
+import com.example.codelearnapp.domain.repository.AchievementRepository
+
 class CheckAchievementsUseCase(private val repository: AchievementRepository) {
     suspend operator fun invoke(lessonsCompleted: Int, totalXp: Int, currentStreak: Int) {
         repository.checkAndUnlockAchievements(lessonsCompleted, totalXp, currentStreak)
--- app/src/main/java/com/example/codelearnapp/domain/usecase/GetAchievementsUseCase.kt
+++ app/src/main/java/com/example/codelearnapp/domain/usecase/GetAchievementsUseCase.kt
@@ -1,6 +1,7 @@
 package com.example.codelearnapp.domain.usecase
 
 import com.example.codelearnapp.data.local.entity.AchievementEntity
+import com.example.codelearnapp.domain.repository.AchievementRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetAchievementsUseCase(private val repository: AchievementRepository) {
